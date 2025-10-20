7d871ce8bef92ecae8b4034036b95758019bb188
Alan Ambrose <alanambrosedev@gmail.com>
2025-10-20 21:33:23 +0530

    Added basic routes and example view for RouteMaster Blog

---
+++ resources/views/page.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>New Page</title>
+</head>
+
+<body>
+    <h1>Dynamic Routing Coming Soon!</h1>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,52 @@
 
 use Illuminate\Support\Facades\Route;
 
+/*
+Route::get() — handles a GET request (the type browsers make by default).
+'/' — the URL path (home page).
+The function() — what to run when someone visits that URL.
+*/
 Route::get('/', function () {
+    return 'Welcome to RouteMaster Blog';
+});
+
+//Laravel automatically detects the response type and sets the headers
+//plain text response
+Route::get('/hello', function () {
+    return 'Hello, Laravel Routing!';
+});
+
+//auto-converted to json by laravel
+Route::get('/array', function () {
+    return ['framework' => 'Laravel', 'version' => app()->version()];
+});
+
+//explicitly returned json
+Route::get('/json', function () {
+    return response()->json([
+        'status' => 'success',
+        'message' => 'This is a json snippet'
+    ]);
+});
+
+//Make a simple blade view and return it via route
+Route::get('/home', function () {
     return view('welcome');
+});
+
+/*
+Create a route /about that returns a short HTML string like
+"This is RouteMaster Blog, built with Laravel 12."
+*/
+Route::get('/about', function () {
+    return "This is RouteMaster Blog, built with Laravel 12.";
+});
+
+//Create a route /info that returns an array:
+Route::get('/info', function () {
+    return ["app" => "RouteMaster Blog", "developer" => "Your Name"];
+});
+
+Route::get('/page', function () {
+    return view('page');
 });
